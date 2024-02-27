4a80f479869d1030b6145a5a88292e68d0177c93
Carlos <dogmalcarlos@gmail.com>
2024-02-27 16:07:59 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const dia = document.getElementById("dia");
+const hora = document.getElementById("hora");
+const minuto = document.getElementById("minuto");
+const segundos = document.getElementById("segundos");
+
+function atualizandoTempo() {
+  // Data final desejada
+  let dataFinal = new Date("2024-05-26");
+
+  // Data atual
+  let dataAtual = new Date();
+
+  // Calcula a diferença entre as datas em milissegundos
+  let diferencaEmMilissegundos = dataFinal - dataAtual;
+
+  // Converte a diferença de milissegundos para dias, horas, minutos e segundos
+  let segundosAtual = Math.floor(diferencaEmMilissegundos / 1000) % 60;
+  let minutosAtual = Math.floor(diferencaEmMilissegundos / (1000 * 60)) % 60;
+  let horasAtual = Math.floor(diferencaEmMilissegundos / (1000 * 60 * 60)) % 24;
+  let diasAtual = Math.floor(diferencaEmMilissegundos / (1000 * 60 * 60 * 24));
+
+  // colocando na tela
+  dia.textContent = `${diasAtual} Dia`;
+  hora.textContent = `${horasAtual} Hora`;
+  minuto.textContent = `${minutosAtual} Min`;
+  segundos.textContent = `${segundosAtual} Seg`;
+}
+
+atualizandoTempo();
+
+setInterval(atualizandoTempo, 1000);
